50e19a5c4c48740f44efbe886f9579ff97ed0b20
wgaliano <wgaliano22@fed.idserve.net>
2023-11-04 23:40:11 +0100

    home_screen created; global_controller implemented

---
+++ lib/controllers/global_controller.dart
@@ -0,0 +1,63 @@
+import 'package:geolocator/geolocator.dart';
+import 'package:get/get.dart';
+
+class GlobalController extends GetxController {
+  //variables
+  final RxBool _isLoading = true.obs;
+  final RxDouble _latitude = 0.0.obs;
+  final RxDouble _longitude = 0.0.obs;
+
+  //istances
+  RxBool checkLoading() => _isLoading;
+  RxDouble getLatitude() => _latitude;
+  RxDouble getLongitude() => _longitude;
+
+  @override
+  void onInit() {
+    getLocation();
+    super.onInit();
+  }
+
+  getLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the 
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale 
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+    
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately. 
+      return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+    } 
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high)
+      .then((value) => {
+        _isLoading.value = false,
+        _latitude.value = value.latitude,
+        _longitude.value = value.longitude
+      });
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+import 'package:weather_app/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +12,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const GetMaterialApp(
+      home: HomeScreen(),
+      title: "Weather",
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
